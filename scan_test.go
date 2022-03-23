73753e46846fc1d00bf65e5637d3fb0d6118439b
Cristhyano <crisdpa@tuta.io>
2022-03-23 10:05:20 -0300

    Fix scan_test.go

--- scan_test.go
+++ scan_test.go
@@ -25,21 +25,21 @@ func TestCleanStrings(t *testing.T) {
 	}
 }
 
-func TestStrToFloat(t *testing.T) {
+func TestCommaToPeriod(t *testing.T) {
 	var tests = []struct {
 		flt  string
-		want float64
+		want string
 	}{
-		{"123,20", 123.20},
-		{"10,5", 10.5},
+		{"123,20", "123.20"},
+		{"10,5", "10.5"},
 	}
 
 	for _, tt := range tests {
 		testname := fmt.Sprintf("%s", tt.flt)
 		t.Run(testname, func(t *testing.T) {
-			ans := strToFloat(tt.flt)
+			ans := commaToPeriod(tt.flt)
 			if ans != tt.want {
-				t.Errorf("got %f, want %f", ans, tt.want)
+				t.Errorf("got %s, want %s", ans, tt.want)
 			}
 		})
 	}
